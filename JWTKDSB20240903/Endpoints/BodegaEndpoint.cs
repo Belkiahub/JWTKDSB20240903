cf14397cfbc00c900b1b58a5c45b35d74fa18e79
KevinSilva <kevinsilva462@gmail.com>
2024-09-03 19:40:55 -0600

    ya edita la bodega

--- JWTKDSB20240903/Endpoints/BodegaEndpoint.cs
+++ JWTKDSB20240903/Endpoints/BodegaEndpoint.cs
@@ -1,47 +1,47 @@
-﻿using System.Xml.Linq;
+﻿using System.Dynamic;
 
 namespace JWTKDSB20240903.Endpoints
 {
-    
-    
-        public static class BodegaEndpoint
+    public static class BodegaEndpoint
+    {
+        static List<ExpandoObject> data = new List<ExpandoObject>();
+
+        public static void AddBodegaEndpoints(this WebApplication app)
         {
-            static List<object> data = new List<object>();
-
-            public static void AddBodegaEndpoints(this WebApplication app)
+            app.MapGet("/bodega", () =>
             {
-                app.MapGet("/bodega", () =>
+                return data;
+            }).RequireAuthorization();
+
+            app.MapPost("/bodega", (int id, string nombre, string ubicacion) =>
+            {
+                dynamic bodega = new ExpandoObject();
+                bodega.id = id;
+                bodega.nombre = nombre;
+                bodega.ubicacion = ubicacion;
+
+                data.Add(bodega);
+                return Results.Ok();
+            }).RequireAuthorization();
+
+            app.MapPut("/bodega/{id}", (int id, string nombre, string ubicacion) =>
+            {
+                // Buscando la bodega por id
+                var bodega = data.FirstOrDefault(b =>
+                    ((IDictionary<string, object>)b)["id"].Equals(id));
+
+                if (bodega == null)
                 {
+                    return Results.NotFound();
+                }
 
-                    return data;
-                }).RequireAuthorization();
+                // Actualizando las propiedades dinámicamente
+                var bodegaDict = (IDictionary<string, object>)bodega;
+                bodegaDict["nombre"] = nombre;
+                bodegaDict["ubicacion"] = ubicacion;
 
-                app.MapPost("/bodega", (int id, string nombre, string ubicacion) =>
-                {
-                    data.Add(new { id, nombre, ubicacion });
-                    return Results.Ok();
-                }).RequireAuthorization();
-
-                app.MapPut("/bodega/{id}", (int id, string nombre, string ubicacion) =>
-                {
-                    var bodega = data.FirstOrDefault(b => (int)((Dictionary<string, object>)b)["id"] == id);
-
-                    if (bodega == null)
-                    {
-                        return Results.NotFound();
-                    }
-
-                    var bodegaDict = (Dictionary<string, object>)bodega;
-
-                    bodegaDict["nombre"] = nombre;
-                    bodegaDict["ubicacion"] = ubicacion;
-
-                    return Results.Ok(bodegaDict);
-                }).RequireAuthorization();
-
-
-
+                return Results.Ok(bodega);
+            }).RequireAuthorization();
         }
     }
-    
 }
